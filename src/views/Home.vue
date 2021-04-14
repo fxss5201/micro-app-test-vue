f5c157ef78e9f1137d235be6ab96fa68e946b3c3
fanyushun <fanyushun@hasng.cn>
2021-04-14 14:00:57 +0800

    子应用+基本actions通信方式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,36 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import actions from '@/shared/actions'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted () {
+    console.log('11111111', window.__POWERED_BY_QIANKUN__)
+    if (window.__POWERED_BY_QIANKUN__) {
+      // 注册观察者函数
+      // onGlobalStateChange 第二个参数为 true，表示立即执行一次观察者函数
+      actions.onGlobalStateChange(state => {
+        console.log('state', state)
+        const { token } = state
+        // 未登录 - 返回主页
+        if (!token) {
+          console.log('未检测到token！')
+          return
+        }
+
+        // 获取用户信息
+        console.log('token', token)
+      }, true)
+
+      setTimeout(() => {
+        const token = 'token123456'
+        actions.setGlobalState({ token })
+      }, 5000)
+    }
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const { name } = require('./package')
+module.exports = {
+  devServer: {
+    // 监听端口
+    port: 8111,
+    // 关闭主机检查，使微应用可以被 fetch
+    disableHostCheck: true,
+    // 配置跨域请求头，解决开发环境的跨域问题
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      jsonpFunction: `webpackJsonp_${name}`
+    }
+  }
+}
