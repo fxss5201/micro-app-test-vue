6823a0bd2232dfe9f69713bfd5ecda3ac591a123
fanyushun <fanyushun@hasng.cn>
2021-04-20 16:21:07 +0800

    eventBus 通信

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
         <el-button size="mini" @click="setMicroActionToken">设置actionToken为microActionToken</el-button>
       </div>
       <div class="content">
-        <span>子应用Props(vuex)通信:</span>
+        <span>子应用Props(bus)通信:</span>
         <el-divider direction="vertical"></el-divider>
-        <span>vuexToken: {{ vuexToken }}</span>
+        <span>busToken: {{ busToken }}</span>
         <el-divider direction="vertical"></el-divider>
-        <el-button size="mini" @click="setMicroVuexToken">设置vuexToken为microVuexToken</el-button>
+        <el-button size="mini" @click="setMicroBusToken">设置busToken为microBusToken</el-button>
       </div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -28,22 +28,15 @@ export default {
   name: 'app',
   data () {
     return {
-      actionToken: '',
-      vuexToken: ''
+      actionToken: ''
+    }
+  },
+  computed: {
+    busToken () {
+      return this.$store.state.token
     }
   },
   mounted () {
-    /**
-      由于在 main.js 中 const { container, store = microStore } = props
-      store 如果独立运行用的是自己的Store，如果作为子应用运行，则用的是主应用的Store，此时主应用的Store在子应用中
-      不是动态的，所以只能初始化赋值，再订阅主应用Store的mutation来修改当前数据
-
-      为了统一处理，可能都需要通过初始化赋值，再订阅主应用Store的mutation来修改当前数据的方式
-    */
-    // 初始化拿到值
-    this.vuexToken = this.$store.state.token
-
-    console.log(this.$store)
     console.log('window.__POWERED_BY_QIANKUN__', window.__POWERED_BY_QIANKUN__)
     if (window.__POWERED_BY_QIANKUN__) {
       actions.onGlobalStateChange((state, prevState) => {
@@ -56,13 +49,8 @@ export default {
       }, true)
     }
 
-    // 订阅 store 的 mutation
-    this.$store.subscribe((mutation, state) => {
-      console.log(mutation.type)
-      console.log(mutation.payload)
-      console.log(state)
-      console.log(this.$store)
-      this.vuexToken = state.token
+    this.$bus.$on('setBusToken', (val) => {
+      this.$store.commit('setToken', val)
     })
   },
   methods: {
@@ -72,12 +60,13 @@ export default {
       }
     },
 
-    setMicroVuexToken () {
-      this.$store.commit('setToken', 'microVuexToken')
+    setMicroBusToken () {
+      this.$store.commit('setToken', 'microBusToken')
+      this.$bus.$emit('setBusToken', 'microBusToken')
     }
   },
   watch: {
-    vuexToken (val, oldVal) {
+    busToken (val, oldVal) {
       console.log('子应用vuex中token值改变前的值为 ', oldVal)
       console.log('子应用vuex中token值改变后的值为 ', val)
     }
